a2df4a7bd4fcc55aed3094ad85f07fd796197f87
Janne Kuuskeri <janne.kuuskeri@gmail.com>
2010-06-29 17:16:48 +0300

    Removed obsolete stuff from grokeutil and added test for it

--- test/util.test.js
+++ test/util.test.js
@@ -1,86 +1,42 @@
-/*
- * 8.4.2009 jedi
- *
- * test utilities
- */
+// -*- coding: utf-8 -*-
+//util.test.js ---
+//
+// Copyright (C) MIT License
+// Tampere University of Technology
+//
+// Created: Tue Jun 29 14:48:36 2010 (+0300)
+// Author: wuher (jedi@cs.tut.fi)
+//
+//
+//
 
-/*jslint onevar: true, undef: true, eqeqeq: true, forin: true,
- plusplus: true, bitwise: true, newcap: true, evil: true */
-/*global exports groke test */
 
 
+var nutil = require("util");
 var util = require('grokeutil');
-var assert = require("assert");
+var assert = require("test/assert");
 
 
-exports.testDateTime = function () {
-    var d = util.datetime();
-    d.setTime(0);
-    assert.equal(d.getTimeStr(), '1970-01-01 02:00:00:000', 'setTime(0)');
-    d.resetTime();
-    assert.notEqual(d.getTimeStr(), '1970-01-01 02:00:00:000', 'resetTime()');
-};
-
-
-exports.testGlobal = function () {
-    var p, globs, global, arr = require('util').array;
-
-    global = util.getGlobalScope();
-    globs = ["system", "print", "require", "installed"];
-
-    for (p in global) {
-        if (!arr.has(globs, p)) {
-            assert.fail(p + " shouldn't be in global scope");
-        }
-    }
-
-    for (p in globs) {
-        if (arr.has(global, p)) {
-            assert.fail(p + " should be in global scope");
-        }
-    }
-};
-
-
-exports.testIsArray = function () {
-    assert.ok(util.isArray([]));
-    assert.ok(util.isArray([3]));
-    assert.ok(util.isArray([3, 'h']));
-    assert.ok(!util.isArray());
-    assert.ok(!util.isArray(3));
-    assert.ok(!util.isArray({}));
-};
-
-
-exports.testObjectEquality = function () {
-    assert.ok(util.isEqual());
-    assert.ok(util.isEqual(3, 3));
-    assert.ok(util.isEqual('', ''));
-    assert.ok(util.isEqual('jedi', 'jedi'));
-    assert.ok(!util.isEqual('-jedi', 'jedi'));
-    assert.ok(util.isEqual(false, false));
-    assert.ok(util.isEqual(true, true));
-    assert.ok(!util.isEqual(false, true));
-    assert.ok(!util.isEqual(true, 1));
-    assert.ok(!util.isEqual(1, true));
-    assert.ok(!util.isEqual(1, false));
-    assert.ok(!util.isEqual(0, false));
-    assert.ok(!util.isEqual('', false));
-    assert.ok(!util.isEqual(false, ''));
-    assert.ok(util.isEqual([], []));
-    assert.ok(util.isEqual({}, {}));
-    assert.ok(!util.isEqual({}, []));
-    assert.ok(!util.isEqual([], {}));
-    assert.ok(!util.isEqual({id: 3}, {id: 3, pid: [2]}));
-    assert.ok(util.isEqual({id: 3, pid: [2]}, {id: 3, pid: [2]}));
-    assert.ok(util.isEqual([{id: 3, k: {s: '3'}}, [3, 3, []]],
-                                 [{id: 3, k: {s: '3'}}, [3, 3, []]]));
-    assert.ok(!util.isEqual([{id: 3, k: {s: '3'}}, [3, 3, ['']]],
-                                  [{id: 3, k: {s: '3'}}, [3, 3, []]]));
-    assert.ok(!util.isEqual(function () {}, function () {}));
-};
+nutil.forEachApply(
+    [
+        ["", ""]
+        , ["/", ""]
+        , ["///", ""]
+        , ["/hii", "hii"]
+        , ["hoo/", "hoo"]
+        , ["/hiihoo/", "hiihoo"]
+    ],
+    function (given, expected) {
+        exports["testTrimslash: " + given] = function () {
+            assert.isSame(expected, util.trimslash(given));
+        };
+    });
 
 
 if (require.main == module.id) {
-    require("os").exit(require("test/runner").run(exports));
+    require("test/runner").run(exports);
 }
+
+
+//
+//util.test.js ends here
